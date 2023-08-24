7c92d1c3e6ac3f27bfc3c52bc60d925c1f314365
Adam Russell <ac.russell@live.com>
2023-08-24 17:24:51 -0400

    added a .gitignore

--- script/graphic.pl
+++ script/graphic.pl
@@ -1,5 +1,3 @@
-#!/usr/bin/perl -w
-
 # Copyright 2000-2002 Raphaël Hertzog, Fabien Fulhaber
 # This file is distributed under the terms of the General Public License
 # version 2 or (at your option) any later version.
